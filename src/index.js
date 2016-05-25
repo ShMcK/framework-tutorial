6cc03705eda73c3cb8106724d619c5b92c40b369
ShMcK <shawn.j.mckay@gmail.com>
2016-05-25 14:47:47 -0700

    Step 3.1: basic render of todos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ export default class TodoList {
     this.todos = titles.map(title => {
       return {title, completed: false};
     }) || [];
+
+    // select the DOM target
+    let target = document.getElementById('todo-list');
+    // create a new HTML Element
+    let element = document.createElement('div');
+    // change the inner HTML
+    element.innerHTML = `<ul class='todo-list'>` +
+      this.todos.map((todo) => {
+        	return `<li>
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>${todo.title}</label>
+              <button class="destroy"></button>
+            </div>
+        	</li>`
+    }).join('') + `</ul>`;
+    // add your new element to the DOM
+		target.appendChild(element);
   }
   add(title) {
     this.todos.push({title, completed: false});
@@ -20,4 +38,8 @@ export default class TodoList {
   }
 }
 
-export let todoList = new TodoList();
+export let todoList = new TodoList(
+  'wash the dishes',
+  'take out garbage',
+  'mow the lawn'
+);
