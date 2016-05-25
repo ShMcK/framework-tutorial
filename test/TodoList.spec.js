44d08c568c6b6f2d99507eae8ac61128fda18a64
ShMcK <shawn.j.mckay@gmail.com>
2016-05-25 11:16:58 -0700

    Test 1.2

--- test/TodoList.spec.js
+++ test/TodoList.spec.js
@@ -1,6 +1,10 @@
 import test from 'ava';
-import TodoList from '../src/index';
+import TodoList, {todoList} from '../src/index';
 
 test('create a class', t => {
 	t.is(typeof TodoList, 'function');
 });
+
+test('todoList is instance of TodoList', t => {
+	t.is(todoList instanceof TodoList, true);
+})
